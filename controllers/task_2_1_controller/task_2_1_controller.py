ba762afe6ce1fce2567023e09dd8e2a09a564ce7
Minseok Kang <mikang@uos.de>
2021-02-25 16:30:01 +0100

    2_1 done up to precison of odometry

--- controllers/task_2_1_controller/task_2_1_controller.py
+++ controllers/task_2_1_controller/task_2_1_controller.py
@@ -4,6 +4,7 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 import math
+from odometry import robot_path
 
 # create the Robot instance.
 robot = Robot()
@@ -32,93 +33,26 @@ motor_right.setVelocity(0.0)
 # Start your code here!
 l = 53
 diam = 40
-pos = [210, 90, 2 - (math.pi/2)]
+pos = [210, 90, math.pi - (math.pi/2)]
 target = [190, 250]
 lamda = 0.1  # robot's turning speed
 psi = math.atan2(target[1] - pos[1], pos[0] - target[0])  # angle to target w.r.t. 0
-epsilon = 30  # error margin
+print(psi)
+epsilon = 5  # error margin
 old_right = 0
 old_left = 0
 
 
-# d_pi = -lamda * math.sin((pos[2] - psi))  # how much the robot should turn
-# if d_pi >= 0:
-#     motor_right.setVelocity(1.0)
-#     motor_left.setVelocity(-1.0)
-# else:
-#     motor_right.setVelocity(-1.0)
-#     motor_left.setVelocity(1.0)
-#
-# desired_wheel_travel = abs(d_pi) * l / 2  # how much the wheel should travel so that robot turns d_pi
-# accum = 0
-# while accum < desired_wheel_travel:
-#     robot.step(timestep)
-#     accum += abs(encoder_right.getValue() - old_right)
-#     print(accum)
-#     old_right = encoder_right.getValue()
-#
-# motor_left.setVelocity(0.0)
-# motor_right.setVelocity(0.0)
+path = robot_path(pos)
 
-
-# robot.step(timestep)
-while math.hypot(pos[0]-target[0], pos[1]-target[1]) > epsilon:  # until convergence
-    d_pi = -lamda * math.sin((pos[2] - psi))  # how much the robot should turn
-    if d_pi >= 0:
-        motor_right.setVelocity(2.0)
-        motor_left.setVelocity(-1.0)
-    else:
-        motor_right.setVelocity(-1.0)
-        motor_left.setVelocity(2.0)
-
-    desired_wheel_travel = abs(d_pi) * l / 2  # how much the wheel should travel so that robot turns d_pi
-    accum = 0
-    while accum < desired_wheel_travel:
-        robot.step(timestep)
-
-        dleft = encoder_left.getValue() - old_left
-        dright = encoder_right.getValue() - old_right
-        old_left = encoder_left.getValue()
-        old_right = encoder_right.getValue()
-        # print(dleft, dright)
-
-        accum += abs(dright)
-
-        # update position
-        r = ((dright + dleft) * l) / ((dright - dleft) * 2)  # eq 6
-        dtheta = (dright - dleft) * diam / (2 * l)  # eq 7
-        dx_ego = r * math.sin(dtheta)  # eq 9
-        dy_ego = r * (1 - math.cos(dtheta))  # eq 10
-        pos[0] -= dx_ego * math.cos(pos[2]) - dy_ego * math.sin(pos[2])  # eq 12, 13, 11
-        pos[1] += dx_ego * math.sin(pos[2]) + dy_ego * math.cos(pos[2])
-        pos[2] += dtheta
-
-
-motor_left.setVelocity(0.0)
-motor_right.setVelocity(0.0)
-
-# l = 53
-# pos = [210, 90, math.pi - (math.pi/2)]
-# target = [50, 60]
-# lamda = 0.1  # robot's turning speed
-# psi = math.atan2(target[1] - pos[1], pos[0] - target[0])  # angle to target w.r.t. 0
-# epsilon = 0.01  # error margin
-# dt = timestep * 0.001
-# print('psi in deg', math.degrees(psi))
-#
-# while abs(pos[2] - psi) > epsilon:
-#     # Compute the necessary wheel velocity
-#     v = (l * lamda * (psi - pos[2])) / (2 * dt)
-#     print(psi - pos[2])
-#     # Turn the robot
-#     motor_left.setVelocity(-v)
-#     motor_right.setVelocity(v)
-#
-#     # Keep track of position, eq 17
-#     pos[2] += (-lamda * (pos[2] - psi)) * dt
-#     print(pos[2] + (math.pi/2))
-#
-#     robot.step(timestep)
+while math.hypot(target[1]-pos[1], pos[0]-target[0]) > epsilon:
+    robot.step(timestep)
+    d_pi = - lamda * math.sin(pos[2] - psi)  # how much the robot should turn in a single timestep
+    v = l * d_pi / 2  # motor velocity at current time
+    motor_left.setVelocity(1-v)
+    motor_right.setVelocity(1+v)
+    pos = path.step(encoder_left, encoder_right)
+    
 
 motor_left.setVelocity(0.0)
 motor_right.setVelocity(0.0)
